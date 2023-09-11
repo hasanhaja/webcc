ec0f713070978996999cd77e6477e9af91ca9476
Hasan Ali <muhammadhasan.ali@cancer.org.uk>
2023-09-11 22:17:30 +0100

    Add temporary prop handling

---
+++ main.js
@@ -0,0 +1,118 @@
+import { WebC } from "@11ty/webc";
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
+
+const sanitizeMarkup = (snippet) => {
+  let result = snippet;
+  result = result.replaceAll("class=", "className=");
+  return result;
+};
+
+const formatProps = (props) => {
+  const types = Object.entries(props).map(([ prop, propType ]) => `${prop}: ${propType}`).join(", ");
+  const params = Object.keys(props).map((prop) => `${prop}`).join(", ");
+
+  return {
+    types: `{${types}}`,
+    params:`{ ${params} }`,
+  };
+};
+
+const toReactComponent = ({ snippet, styles, name, props }) => {
+  const { types, params } = formatProps(props);
+  const boilerplate = `
+    // This component was auto-generated from a WebC template
+
+    ${ styles ? `import "./index.css";` : ""}
+
+    type ${name}Props = ${types};
+
+    export const ${name} = (${params}) => (
+      <>
+        ${sanitizeMarkup(snippet)}
+      </>
+    );
+
+    export default ${name};
+  `;
+  
+  return {
+    component: boilerplate,
+    styles,
+    name,
+  };
+};
+
+const toReactComponentFile = async ({ component, name, styles, outputDir}) => {
+  const componentFileName = "index.tsx";
+  const stylesFileName = "index.css";
+  const componentDir = `./${outputDir}/components/${name}`;
+  
+  const componentDirUrl = new URL(componentDir, import.meta.url);
+  await mkdir(componentDirUrl, { recursive: true });
+
+  await writeFile(`${componentDir}/${componentFileName}`, component);
+
+  if (styles) {
+    await writeFile(`${componentDir}/${stylesFileName}`, stylesData);
+  }
+};
+
+const capitalize = (word) => word[0].toUpperCase() + word.substr(1).toLowerCase();
+
+const getComponentName = (path) => {
+  const regex = /\/([^/]+)\.webc$/;
+  const match = path.match(regex);
+  
+  if (match) {
+    const componentName = match[1];
+
+    const name = componentName.split("-")
+      .map(capitalize)
+      .join("");
+    return name;
+  }
+
+  return undefined;
+};
+
+const compile = async (path, schema) => {
+  let page = new WebC();
+
+  // This enables aggregation of CSS and JS
+  // As of 0.4.0+ this is disabled by default
+  page.setBundlerMode(true);
+  page.defineComponents("./components/**.webc");
+
+  const filePath = new URL(path, import.meta.url);
+  const schemaPath = new URL(schema, import.meta.url);
+  const contents = await readFile(filePath, { encoding: "utf8" });
+  page.setContent(contents);
+
+  const propsContents = await readFile(schemaPath, { encoding: "utf8" });
+  const props = JSON.parse(propsContents);
+  const data = Object.fromEntries(Object.keys(props).map((key) => [`${key}`, `{${key}}`]));
+
+  let { html, css, js, components } = await page.compile({
+    data,
+  });
+
+  // console.log("----HTML----");
+  // console.log(html);
+  // console.log("------------");
+
+  // console.log("-----CSS----");
+  // console.log(path);
+  // console.log("------------");
+
+  return {
+    snippet: html,
+    styles: css[0],
+    name: getComponentName(path) ?? "Component",
+    props,
+  }
+};
+
+const result = await compile("./components/component-with-props.webc", "./components/component-with-props.json");
+
+await toReactComponentFile({...toReactComponent(result), outputDir: "output", });
+
---
+++ output/components/ComponentWithProps/index.tsx
@@ -0,0 +1,18 @@
+
+    // This component was auto-generated from a WebC template
+
+    
+
+    type ComponentWithPropsProps = {greeting: string};
+
+    export const ComponentWithProps = ({ greeting }) => (
+      <>
+        <div>
+  <p>{greeting}</p>
+</div>
+
+      </>
+    );
+
+    export default ComponentWithProps;
+  
